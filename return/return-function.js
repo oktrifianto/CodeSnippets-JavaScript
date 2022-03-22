f34a1692c873edd17fbecc96fbf8e1d9e97d6892
Rahmat <oktrifianto@gmail.com>
2022-03-22 15:09:38 +0700

    example `return` using ES5

--- return/return-function.js
+++ return/return-function.js
@@ -14,3 +14,17 @@ console.log(multiTwo(2)); // 2 * 3 = 6
 console.log(multiTwo(3)); // 3 // only 3
 console.log(multiTwo(4)); // 12
 
+////////////////////////////////////////////////////
+/**
+ * @description   More handling return function (ES5)
+ * @param         {*} val 
+ * @returns       val
+ */
+function multiFour(val) {
+  if (val != 4) return val*4;
+  return val;
+}
+
+console.log(multiFour(2)); // 2 * 4 = 8
+console.log(multiFour(4)); // keep 4
+console.log(multiFour(6)); // 6 * 4 = 24
