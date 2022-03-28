2ff42004b76c2e36cea9172499b8fe63d4aed034
Rahmat <oktrifianto@gmail.com>
2022-03-28 11:04:30 +0700

    Add `isManual()` method in this class

--- class/CarClass.js
+++ class/CarClass.js
@@ -4,7 +4,7 @@
  * @param {number}  year   - time of car production
  * @param {string}  color  - color of car
  * @param {number}  max_speed  - how fast the car
- * @param {boolean}  manual_trans - is manual transmission?
+ * @param {boolean} manual_trans - is manual transmission?
  */
 class Car {
   constructor(brand, year, color, max_speed, manual_trans){
@@ -18,10 +18,20 @@ class Car {
   getSpeed(){
     return `Your ${this.brand} car have maximum speed ${this.max_speed} km/h`;
   }
+
+  isManual(){
+    if (this.manual_trans){
+      return "Manual Transmission";
+    }
+    return "Automatic Transmission";
+  }
+
 }
 
 // Instance class
-const supra  = new Car("Toyota Supra", 2021, "Black", 200);
+const supra  = new Car("Toyota Supra", 2021, "Black", 200, false);
 
 console.log(supra.color)
 console.log(supra.getSpeed())
+console.log(supra.manual_trans)
+console.log(supra.isManual())
