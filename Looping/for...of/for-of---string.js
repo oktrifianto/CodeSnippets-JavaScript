a5f56c1b689d6d78e32c3121c783d87cb1e9ab57
Rahmat <oktrifianto@gmail.com>
2022-04-03 02:08:36 +0700

    Looping string character using `for...of`

---
+++ Looping/for...of/for-of---string.js
@@ -0,0 +1,18 @@
+/**
+ * Looping character from string using `for...of`
+ */
+const str = "coffee";
+for (const s of str){
+  console.log(s);
+}
+
+/* =============== result ===========
+
+c
+o
+f
+f
+e
+e
+
+*/
