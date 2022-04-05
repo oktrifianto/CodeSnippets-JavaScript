82f04a427b7cdb82db686aed67821bb1a3f60932
Rahmat <oktrifianto@gmail.com>
2022-04-05 22:32:15 +0700

    Example sum data using `for` loop

--- looping/for/1--for-loop.js
+++ looping/for/1--for-loop.js
@@ -17,3 +17,14 @@ for ( let i = 0; i < data.length; i++ ){
 5
 
 */
+
+// ==================
+// Sum using for loop
+// ==================
+const my_data = [12, 34, 23, 38];
+let sum = 0;
+for (let i = 0; i < my_data.length; i++){
+  sum += my_data[i];
+}
+
+console.log(sum); // 107
