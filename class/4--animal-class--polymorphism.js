4060c0a7c722d64cf1ce1821c84cf68476483c15
Rahmat <oktrifianto@gmail.com>
2022-04-05 12:48:39 +0700

    Example `polymorphism` in the animal class

---
+++ class/4--animal-class--polymorphism.js
@@ -0,0 +1,30 @@
+/**
+ * ======================
+ * What is Polymorphism?
+ * ======================
+ * What?  ability to call same method on different object
+ *        and each object responds in different way
+ * @see https://stackoverflow.com/questions/27642239/what-is-polymorphism-in-javascript
+ */
+
+class Animal {
+  constructor(sound){
+    this.sound = sound;
+  }
+
+  // one method
+  showSound(){
+    return `Sound is ${this.sound}`;
+  }
+}
+
+// objects different
+let dog       = new Animal('GugGug');
+let chicken   = new Animal('KokKokKok');
+let duck      = new Animal('WekWekWek');
+
+
+// will responds different ways
+console.log(dog.showSound());
+console.log(chicken.showSound());
+console.log(duck.showSound());
