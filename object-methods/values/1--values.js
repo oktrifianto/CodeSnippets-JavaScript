c2698bf004f10069ba596f2ef4e775e774e57d34
Rahmat <oktrifianto@gmail.com>
2022-04-13 16:46:55 +0700

    Method `values` in JavaScript

---
+++ object-methods/values/1--values.js
@@ -0,0 +1,12 @@
+// create example an object
+const object = {
+  name  : 'Cup',
+  color : 'white',
+  filled: false
+}
+
+// using `values` method
+console.log(Object.values(object)); // [ 'Cup', 'white', false ]
+
+
+console.log(Object.keys(object)); // [ 'name', 'color', 'filled' ]
