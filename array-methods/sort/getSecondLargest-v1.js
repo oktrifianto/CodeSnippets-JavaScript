d39355204cd7d14fba731ecfa65dd2ff92162b79
Rahmat <oktrifianto@gmail.com>
2022-04-17 03:18:30 +0700

    Fix: duplicate value array in the `getSecondLargest` function

--- array-methods/sort/getSecondLargest-v1.js
+++ array-methods/sort/getSecondLargest-v1.js
@@ -6,7 +6,10 @@
 const gsl = nums => {
   if (nums.length < 3) return 'Cannot compare!';
   nums.sort((a, b) => a - b ); // filter sort from small
-  return nums[nums.length - 2]; // get array from behind
+  const unique_nums = nums.filter((item, index) => {
+    return nums.indexOf(item) == index;
+  });
+  return unique_nums[unique_nums.length - 2]; // get array from behind
 }
 
 console.log(gsl([12, 42, 43, 82, 38])); // 43 (√)
@@ -15,6 +18,6 @@ console.log(gsl([12, 34]));
 
 // need to fix?
 // if double value
-console.log(gsl([2, 3, 6, 6, 5])); // failed... output: 6 ... should be 5
+console.log(gsl([2, 3, 6, 6, 5])); // 5 (√) --- done duplicate array
 // CLUE: filter to unique array
 // see : https://stackoverflow.com/questions/38206915/filter-array-to-have-unique-values
