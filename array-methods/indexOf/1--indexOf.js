978dbb7bf479a51b5cea7cdf43a669d9c52fb958
Rahmat <oktrifianto@gmail.com>
2022-04-18 01:21:01 +0700

    Test negative index value in the `indexOf` method

--- array-methods/indexOf/1--indexOf.js
+++ array-methods/indexOf/1--indexOf.js
@@ -19,6 +19,10 @@ const pets = ['duck', 'chicken', 'cat', 'dog', 'betta fish'];
 // console.log(pets);
 // output: [ 'duck', 'chicken', 'cat', 'dog', 'betta fish' ]
 // index : [    0  ,   1      ,   2   ,  3  ,       4      ]
+/**
+ * negative index:
+ *         [    -5 ,   -4     ,   -3  ,  -2 ,       -1     ]
+ */
 
 console.log(pets.indexOf('cat'));  // will get index-2
 // output: 2
@@ -31,3 +35,9 @@ console.log(pets.indexOf('dog', 2)); // start from index-2 [`cat`] - will get in
 
 console.log(pets.indexOf('lion'));  // not found will return -1
 // output: -1
+
+
+console.log(pets[-2]); // undefined
+
+console.log(pets.indexOf('dog', -4));  // start from index (-4)
+// output: 3
