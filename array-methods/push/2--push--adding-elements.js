47ed2a113e8c46a446ba92775ffc07420b7ff241
Rahmat <oktrifianto@gmail.com>
2022-04-20 03:42:45 +0700

    Adding two element using `push` method

---
+++ array-methods/push/2--push--adding-elements.js
@@ -0,0 +1,11 @@
+/** ----------------------------------
+ * Example adding elements to an array
+ * -----------------------------------
+ */
+let drinks = ['coffee', 'milk'];
+let total  = drinks.push('juice', 'pepsi'); // append two element to array
+
+console.log(drinks); 
+// output: [ 'coffee', 'milk', 'juice', 'pepsi' ]
+console.log(total);
+// 4
