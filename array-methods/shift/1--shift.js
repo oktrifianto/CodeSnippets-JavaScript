2c4a0833b69da0b6d29669f15d973c1878c26e5b
Rahmat <oktrifianto@gmail.com>
2022-04-22 00:19:22 +0700

    Example `shift()` method in JavaScript

---
+++ array-methods/shift/1--shift.js
@@ -0,0 +1,19 @@
+/** –––––––––––––––––––––––––––––
+ * `shift()` method in JavaScript
+ * ––––––––––––––––––––––––––––––
+ * @description
+ *  > remove first element from an array
+ *  > return removed element
+ * 
+ * @syntax
+ *  shift()
+ * 
+ */
+let data = [1, 2, 3, 4, 5];
+data.shift(); // remove
+
+console.log(data);
+// [ 2, 3, 4, 5]
+
+console.log(data.shift()); // return removed element
+// return 2
