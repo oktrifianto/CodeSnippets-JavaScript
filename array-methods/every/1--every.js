4fb134f66aa244258db4b8c939cbfa84df7a9983
Rahmat <oktrifianto@gmail.com>
2022-04-23 23:11:59 +0700

    Test another value using `every()` method

--- array-methods/every/1--every.js
+++ array-methods/every/1--every.js
@@ -13,3 +13,10 @@ const data = [1, 3, 4, 20, 45];
 
 console.log(data.every(isBelow)); 
 // true
+
+/**--------------------*/
+// test another value
+const data2 = [2, 45, 38, 100];
+
+console.log(data2.every(isBelow));
+// false (because `data2` contain 100, is greater than 50)
