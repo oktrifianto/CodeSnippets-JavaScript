5430db2407b548a48455f7e160a7538ae199eb23
Rahmat <oktrifianto@gmail.com>
2022-04-25 01:38:17 +0700

    Check `Palindrome` using stack method

---
+++ array-methods/pop/3--pop---checkPalindrome.js
@@ -0,0 +1,36 @@
+/**
+ * Check Palindrome String
+ * @param     {string} value
+ * @return    {boolean}
+ */
+
+const isPalindrome = val => {
+  const NewValue = val.toLowerCase().replace(/\s/g, "");
+
+  let stack = [];
+  for (let i = 0; i < NewValue.length; i++){
+    stack.push(NewValue[i]);
+  }
+
+  // take out
+  let reverseValue = ''; // init string
+  while (stack.length > 0){
+    reverseValue += stack.pop();
+  }
+
+  if (reverseValue == NewValue)
+    return true;
+  return false;
+  
+  // only checking
+  /*
+  return reverseValue
+    .toLowerCase()
+    .replace(/\s/g, "") + " - "+ val.toLowerCase().replace(/\s/g, ""); 
+  */
+}
+
+console.log(isPalindrome('alula')); // true
+console.log(isPalindrome('Anna')); // true  // string must be lowercase
+console.log(isPalindrome('Air an Aria')); // should be `true` // remove all whitespace
+console.log(isPalindrome('asjadjbabj')); // must false
