7c67a5c606a0440eea34a103f928653dc3fa8119
Rahmat <oktrifianto@gmail.com>
2022-04-28 01:44:42 +0700

    Example function property: name

---
+++ function/function-properties/2--func-name.js
@@ -0,0 +1,16 @@
+/** ------------
+ * Function.name
+ * -------------
+ * @description
+ *  a function object read-only name property indicates function name
+ * 
+ * @see   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+ * 
+ * 
+ */
+
+function my_func_1(){
+  return 0;
+}
+
+console.log(my_func_1.name); // my_func_1
