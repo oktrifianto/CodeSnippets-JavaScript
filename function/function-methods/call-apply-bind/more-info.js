2df6dd415eee61807911343af7236c5e3a9ffc4d
Rahmat <oktrifianto@gmail.com>
2022-05-10 10:09:25 +0700

    More different about call, apply, bind

---
+++ function/function-methods/call-apply-bind/more-info.js
@@ -0,0 +1,21 @@
+/**
+ * call() vs apply() vs bind()
+ * ---------------------------
+ */
+
+// ----------- Links -----------
+/*
+https://stackoverflow.com/questions/15455009/javascript-call-apply-vs-bind
+
+
+*/
+
+// -------- More Explanation -------- //
+/*
+call and apply ---> call a function --- immediately
+bind ---> creates a function        --- return later
+
+call    -> pass argument individually
+apply   -> pass argument array 
+
+*/
