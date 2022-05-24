419137f79232d6f4ef0d780cfadb1f18f01f5a25
Rahmat <oktrifianto@gmail.com>
2022-05-25 06:55:17 +0700

    Add syntax example for `find` method

--- array-methods/find/1--find.js
+++ array-methods/find/1--find.js
@@ -5,6 +5,22 @@
  * @description
  *    - returns first element in the provided array
  *    - if no values == returned `undefined`
+ * 
+ * @syntax
+ *    // arrow function
+ *    find((el) => {...})
+ *    find((el, index) => {...})
+ *    find((el, index, array) => {...})
+ * 
+ *    // callback function
+ *    find(callbackFn)
+ *    find(callbackFn, thisArg)
+ * 
+ *    // inline callback function
+ *    find(function(el) {...})
+ *    find(function(el, index) {...})
+ *    find(function(el, index, array) {...})
+ *    find(function(el, index, array) {...}, thisArg)
  */
 const my_array =  [2, 30, 38, 127, 10];
 
