6d5e4cb2d3cfd71ba2748d1d6be0cec396e61065
Rahmat <oktrifianto@gmail.com>
2022-05-27 08:25:02 +0700

    Init `lastIndexOf` method in JavaScript

---
+++ array-methods/lastIndexOf/1--lastIndexOf.js
@@ -0,0 +1,20 @@
+/** ------------------------
+ * `lastIndexOf()` method JS
+ * -------------------------
+ *    returns the last index at which given element can be found in the array
+ *    -1 if it is not present
+ *    array is searched backwards, starting at fromIndex
+ * 
+ *    searching from last
+ */
+
+const drinks = ['tea', 'coffee', 'lemon', 'water', 'greentea'];
+
+console.log(drinks.lastIndexOf('tea')); // 0
+
+console.log(drinks.lastIndexOf('coffee')); // 1
+
+console.log(drinks.lastIndexOf('water')); // 3
+
+
+
