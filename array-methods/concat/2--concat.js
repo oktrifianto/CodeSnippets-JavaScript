1d95bf59bdc03ff6e4c676045862bd8b41e3f368
Rahmat <oktrifianto@gmail.com>
2022-05-29 07:47:34 +0700

    Test more `concat` method in JS

---
+++ array-methods/concat/2--concat.js
@@ -0,0 +1,15 @@
+// --------------------------------- //
+// Concatenating values to an array
+const l = ['a', 'b', 'c'];
+const al = l.concat(1, ['tiger', 'lion']);
+console.log(al);
+// [ 'a', 'b', 'c', 1, 'tiger', 'lion' ]
+
+
+// --------------------------------- //
+// Test nested array
+let b =[[1]];
+console.log(b); // [ [ 1 ] ]
+
+// --------------------------------- //
+
