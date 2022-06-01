06ec59f6b9cd9efa67a17f851ef5d69f1fdeba59
Rahmat <oktrifianto@gmail.com>
2022-06-01 12:30:39 +0700

    Using `charAt()` method in JS

---
+++ string/string-methods/chartAt/1--chartAt.js
@@ -0,0 +1,16 @@
+/** -----------------
+ * `charAt` method JS
+ * ------------------
+ * 
+ * @description
+ *    returns a new string consisting of the single UTF-16 code unit located at the specified offset into the string
+ * 
+ */
+
+// --------  example ---------
+const s = 'Hello you!';
+
+const index = 4;
+
+console.log(`The character at index ${index} is ${s.charAt(index)}`);
+// The character at index 4 is o
