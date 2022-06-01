648855a6a496f8c1be9d94cf577f22250c34158c
Rahmat <oktrifianto@gmail.com>
2022-06-01 09:46:29 +0700

    Length properties on string JS

---
+++ string/string-properties/1--string-length.js
@@ -0,0 +1,11 @@
+/** ------------
+ * String Length
+ * -------------
+ * 
+ *  > length is a read-only data property of string instance
+ */
+const str = 'Hello world';
+const l = str.length;
+
+console.log(`${str} : ${l}`); 
+// Hello world : 11
