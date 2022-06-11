c67c324e4b88d09223ca2c12966d3c94fd0ac474
Rahmat <oktrifianto@gmail.com>
2022-06-11 13:17:17 +0700

    Using `replace` method in JS

---
+++ string/string-methods/replace/1--replace.js
@@ -0,0 +1,9 @@
+/** ---------------------
+ * `replace` method in JS
+ * ----------------------
+ */
+
+const x = 'Hello, it is a cat';
+
+console.log(x.replace('cat', 'dog'));
+// Hello, it is a dog
