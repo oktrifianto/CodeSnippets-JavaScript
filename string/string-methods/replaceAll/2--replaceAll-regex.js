30a36f3fc371e9bf200b07b222f6ee94355cb3aa
Rahmat <oktrifianto@gmail.com>
2022-06-30 08:53:38 +0700

    Using `replaceAll` method and regex

---
+++ string/string-methods/replaceAll/2--replaceAll-regex.js
@@ -0,0 +1,6 @@
+// Using regex in `replaceAll` method
+// ----------------------------------
+
+const x = 'The cat is cute. It is black cat';
+console.log(x.replaceAll(/cat/ig, 'chicken'));
+// Output: The chicken is cute. It is black chicken
